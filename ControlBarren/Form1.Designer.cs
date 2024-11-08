b9ed749d2a021d27a05b191827d99b4b19c6faaf
Christian Cueva <christianecueva@gmail.com>
2024-11-08 13:20:12 -0500

    formcambio

--- ControlBarren/Form1.Designer.cs
+++ ControlBarren/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1582, 853);
+            this.ClientSize = new System.Drawing.Size(800, 500);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
